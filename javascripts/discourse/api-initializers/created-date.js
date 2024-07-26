f6a59bf16cf7dd6f0fb653aaebc03b501ead0f35
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2024-07-26 15:39:15 -0700

    Setup for topic header column component

--- javascripts/discourse/api-initializers/created-date.js
+++ javascripts/discourse/api-initializers/created-date.js
@@ -1,6 +1,7 @@
 import { apiInitializer } from "discourse/lib/api";
 import NavItem from "discourse/models/nav-item";
 import I18n from "I18n";
+// import TopicListHeaderCreatedColumn from "../components/topic-list-header-created-column";
 
 export default apiInitializer("1.30.0", (api) => {
   if (settings.enable_sort_by_created_date_nav_bar_item) {
@@ -23,4 +24,13 @@ export default apiInitializer("1.30.0", (api) => {
       },
     });
   }
+  // note to self: don't use the topic-list-header.gjs
+  // it is behind the experimental glimmer topic list groups
+  // Once it is ready, can use the following:
+  // api.renderInOutlet("topic-list-header-after", TopicListHeaderCreatedColumn);
+
+  // see for alternative:
+  // https://meta.discourse.org/t/new-th-in-topic-table/280116/2
+  // https://meta.discourse.org/t/an-interesting-strategy-for-passing-properties-via-raw-template-plugin-outlets/210054/2
+  // https://meta.discourse.org/t/how-to-implement-raw-plugin-outlet/178385
 });
---
+++ javascripts/discourse/components/topic-list-header-created-column.gjs
@@ -0,0 +1,12 @@
+import Component from "@glimmer/component";
+
+export default class TopicListHeaderCreatedColumn extends Component {
+
+  constructor() {
+    super(...arguments);
+  }
+
+  <template>
+    <span>hello world</span>
+  </template>
+}
